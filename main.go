17513482e5ff8ab714788c7d0558060769f802c4
alpn <alpn@users.noreply.github.com>
2022-01-08 16:30:59 -0600

    Turn this into a tiny static blog generator
    	modified:   main.go

--- main.go
+++ main.go
@@ -2,25 +2,46 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"bufio"
 	"path"
 	"path/filepath"
 	"strconv"
+	"strings"
     "github.com/gomarkdown/markdown"
 )
 
-const htmlPrefix = "<html><body><h2>Here</h2><hr><ul>\n"
-const htmlPostfix = "</ul></body></html>"
+const htmlPrefix = `<html><body><h2>Here</h2><hr><ul>`
+const htmlPostfix = `</ul></body></html>`
+const css = 
+`
+        <style>
+            html {
+              max-width: 70ch;
+              padding: 3em 1em;
+              margin: auto;
+              line-height: 1.75;
+              font-size: 1.25em;
+              font-family: sans-serif;
+            }
+
+            img {
+              max-width: 70ch;
+            }
+            a{
+                text-decoration: none;
+                color : red
+            }
+        </style>`
 
 func requestHandler(w http.ResponseWriter, req *http.Request) {
 
 	filePath := filepath.Join(".", req.RequestURI)
-	fmt.Println(filePath)
+	log.Println(filePath)
 
 	fileStat, err := os.Stat(filePath)
 
@@ -61,54 +82,7 @@ func requestHandler(w http.ResponseWriter, req *http.Request) {
 
 	// Render Markdown files to HTML
 	if path.Ext(filePath) == ".md" {
-		md, err := ioutil.ReadFile(filePath);
-		if err != nil {
-			fmt.Println(err)
-			w.Write([]byte("500"))
-			return;
-		}
-
-		html :=
-        `<head><script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-        <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js"></script>
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/atom-one-dark.min.css">
-        <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script>
-        <style>
-            html {
-              max-width: 70ch;
-              padding: 3em 1em;
-              margin: auto;
-              line-height: 1.75;
-              font-size: 1.25em;
-              font-family: sans-serif;
-            }
-
-            img {
-              max-width: 70ch;
-            }
-            a{
-                text-decoration: none;
-                color : red
-            }
-        </style>
-		<script>
-		window.MathJax = {
-			tex: {
-			  macros: {
-				R : "\\mathbb{R}",
-				map : "#1:#2\\rightarrow#3"
-			  }
-			}
-		  };
-		
-          hljs.highlightAll();
-		  </script></head>
-		`
-		w.Write([]byte(html))
-
-		rendered_md := markdown.ToHTML(md, nil, nil)
-		w.Write(rendered_md)
-
+		handleMarkdownFile(filePath, w)
 		return
 	}
 
@@ -120,12 +94,110 @@ func requestHandler(w http.ResponseWriter, req *http.Request) {
 	}
 
 	if _, err = io.Copy(w, f); err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
 }
 
-func run(portNumber int) error {
+func handleMarkdownFile(filePath string, w io.Writer) error{
+
+	md, err := ioutil.ReadFile(filePath);
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte("500"))
+		return err
+	}
+
+	html :=
+	`<head><script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+	<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js"></script>
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/atom-one-dark.min.css">
+	<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script>
+	<script>
+	window.MathJax = {
+		tex: {
+			macros: {
+			R : "\\mathbb{R}",
+			map : "#1:#2\\rightarrow#3"
+			}
+		}
+		};
+	
+		hljs.highlightAll();
+		</script></head>
+	`
+	html += css
+
+	n,_ := w.Write([]byte(html))
+	log.Println("written: " , strconv.Itoa(n))
+
+	rendered_md := markdown.ToHTML(md, nil, nil)
+	w.Write(rendered_md)
+
+	log.Println("done")
+	return nil
+}
+
+func generateStaticBlog() {
+
+	postsPath := filepath.Join(".", "posts")
+	blogPath := filepath.Join(".", "blog")
+
+	postsDir, postsErr := os.Stat(postsPath)
+	blogDir, blogErr := os.Stat(blogPath)
+
+	if  nil != postsErr || nil != blogErr{
+		return
+	}
+
+	if !postsDir.IsDir() || !blogDir.IsDir() {
+		return
+	}
+
+	files, err := ioutil.ReadDir(postsPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var html = htmlPrefix + css
+
+	for _, file := range files {
+		var fileName = file.Name()
+		if file.IsDir() {
+			log.Fatal("make sure posts dir is flat")
+		}
+
+		filePath := filepath.Join(postsPath, fileName)
+		log.Println("Handling " + filePath)
+
+		htmlPostPath := filepath.Join(blogPath, strings.TrimSuffix(fileName, filepath.Ext(fileName))+".html")
+		f, err := os.Create(htmlPostPath)
+		defer f.Close()
+
+		w := bufio.NewWriter(f)
+
+		err = handleMarkdownFile(filePath, w)
+		if nil != err{
+			log.Fatal(err)
+		}
+
+		w.Flush()
+
+		html += "<li><a href='" + fileName + "'>" + fileName + "</a></li>\n"
+	}
+
+	html += htmlPostfix
+	buffer := []byte(html)
+
+	indexPath := filepath.Join(blogPath, "index.html")
+	err = ioutil.WriteFile(indexPath, buffer, 0644)
+
+	if nil != err {
+		log.Fatal(err)
+	}
+}
+
+func runServer(portNumber int) error {
 
 	path, err := os.Getwd()
 	if err != nil {
@@ -135,8 +207,9 @@ func run(portNumber int) error {
 	portNumberStr := strconv.Itoa(portNumber)
     localhostStr := "localhost"
 
-	fmt.Println("[Here] - serving " + path + " at port " + portNumberStr)
-    fmt.Println("http://" + localhostStr + ":" + portNumberStr)
+	log.Println("[Here]")
+	log.Println("Serving: " + path)
+    log.Println("http://" + localhostStr + ":" + portNumberStr)
 
 	http.HandleFunc("/", requestHandler)
 	if err := http.ListenAndServe(localhostStr + ":"+portNumberStr, nil); err != nil {
@@ -148,13 +221,20 @@ func run(portNumber int) error {
 func main() {
 
 	var portNumber int
+	var makeBlog bool
 
 	flag.IntVar(&portNumber, "port", 9898, "port number to listen on")
+	flag.BoolVar(&makeBlog, "b", false, "generate static blog")
 	flag.Parse()
 
-	if err := run(portNumber); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
+    if makeBlog {
+        log.Println("Generating static blog")
+		generateStaticBlog()
+        return
+    }
+
+    if err := runServer(portNumber); err != nil {
+		log.Fatal(err)
 	}
 
 }
